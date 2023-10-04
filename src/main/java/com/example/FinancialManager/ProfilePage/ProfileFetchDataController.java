096b83e2e28b7c5f07609f534bbbe6122359f233
bartosz <bartosz.zurek.02@gmail.com>
2023-10-04 13:09:03 +0200

    JWT token authentication DB repository fixed login & registration service

--- src/main/java/com/example/FinancialManager/ProfilePage/ProfileFetchDataController.java
+++ src/main/java/com/example/FinancialManager/ProfilePage/ProfileFetchDataController.java
@@ -16,18 +16,12 @@ public class ProfileFetchDataController {
     {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        // You can access various user details from the authentication object
-        String username = authentication.getName(); // Get the username
-        boolean isAuthenticated = authentication.isAuthenticated(); // Check if the user is authenticated
+        String username = authentication.getName();
         String a="";
-        // You can also access custom user details if they are available in the authentication object
-        // For example, if you have a custom UserDetails implementation
         if (authentication.getPrincipal() instanceof AppUser) {
             AppUser customUserDetails = (AppUser) authentication.getPrincipal();
             a = customUserDetails.getFirstName();
         }
-
-        // You can return the user data as JSON or in any other desired format
         return username + "data: " + a;
     }
 }
